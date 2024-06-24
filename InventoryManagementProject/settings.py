d8d106adea336f3fae8cd239564683c676e63ec6
JunaidAbbas <151387646+JunaidAbbasKaladgi@users.noreply.github.com>
2024-06-24 15:40:34 +0530

    Update settings.py

--- InventoryManagementProject/settings.py
+++ InventoryManagementProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c=s9tjm^5we4b*ie!s!nfb$77^eui9-#m=*%5korugo=e0)se$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
